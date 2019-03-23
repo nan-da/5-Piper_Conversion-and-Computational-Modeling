57f294324f466b2f05621d4f29b00af08bfe9e8e
Nan Da <32020486+nan-da@users.noreply.github.com>
2019-03-23 15:40:09 -0500

    Add plotting functions

--- Schinke_Latin_Stemming.R
+++ Schinke_Latin_Stemming.R
@@ -23,11 +23,11 @@ figure_6b = c('iuntur','beris', 'erunt', 'untur', 'iunt', 'mini', 'ntur',
               'stis', 'bor', 'ero', 'mur', 'mus', 'ris', 'sti', 'tis', 
               'tur', 'unt', 'bo', 'ns', 'nt', 'ri', 'm', 'r', 's', 't')
 
-convert_2 = c( c(c('iuntur', 'erunt', 'untur', 'iunt', 'unt'), 'i'),
-               c(c('beris', 'bor', 'bo'), 'bi'),
-               c(c('ero'), 'eri') )
+convert_2 = list( i=c('iuntur', 'erunt', 'untur', 'iunt', 'unt'),
+                  bi=c('beris', 'bor', 'bo'),
+                  eri=c('ero') )
 
-schinke_latin_stemming <- function(string, noun_or_verb_form='noun'){
+schinke_latin_stemming <- function(string, noun_or_verb_form='verb'){
   for (convert_pair in convert_1){
     string = gsub(convert_pair[1], convert_pair[2],string)
   }
@@ -49,10 +49,11 @@ schinke_latin_stemming <- function(string, noun_or_verb_form='noun'){
       return( noun )
   }
   else if(noun_or_verb_form == 'verb'){
-    for (pair in convert_2)
-      for (suffix in pair[1])
-          string = sub(paste(suffix,'$',sep = ""),pair[2],string)
-    
+    for (key in names(convert_2)){
+      value = convert_2[[key]]
+      for (suffix in value)
+          string = sub(paste(suffix,'$',sep = ""),key,string)
+    }
     for (suffix in figure_6b){
       verb = sub(paste(suffix,'$',sep = ""),'',string)
       if(verb!=string)
@@ -82,22 +83,56 @@ schinke_latin_stemming_passage <- function(passage){
 in_cross_half <- function(freq.dist, half_cut_point){
   a=0 # in half
   c=0 # cross half
+  a_scale=c()
+  c_scale=c()
   n = nrow(freq.dist)
   for (i in 1:half_cut_point){
-    for (j in 1:i)
+    for (j in 1:i){
       a=a+freq.dist[i,j]
-    for (j in (half_cut_point+1):n)
+      a_scale = c(a_scale, freq.dist[i,j])
+    }
+    for (j in (half_cut_point+1):n){
       c=c+freq.dist[i,j]
+      c_scale = c(c_scale, freq.dist[i,j])
+    }
   }
-  
+  a = a/( half_cut_point*(half_cut_point-1)/2 )
   # a=a/40
-  c=c/half_cut_point^2
+
   b=0
-  for (i in (half_cut_point+1):n)
-    for (j in (half_cut_point+1):i)
+  for (i in (half_cut_point+1):n){
+    for (j in (half_cut_point+1):i){
       b=b+freq.dist[i,j]
+      a_scale=c(a_scale,freq.dist[i,j])
+    }
+  }
   # b=b/40
-  contraction = (a-b)/( half_cut_point*(half_cut_point-1)/2 )
-  print(c("In-half: ", contraction, "cross-half:", c))
+  b = b/( (n-half_cut_point)*(n-half_cut_point-1)/2 )
+  contraction = a-b
+  c=c/(half_cut_point*(n-half_cut_point))
+  x=scale(a_scale)
+  y=scale(c_scale)
+  
+  # print(c("Sample Number In-half:", half_cut_point*(half_cut_point-1)/2,(n-half_cut_point)*(n-half_cut_point-1)/2, "Cross-half:",half_cut_point*(n-half_cut_point)))
+  # print(c('z-score:',(contraction-attr(x,"scaled:center"))/attr(x,"scaled:scale"), (c-attr(y,"scaled:center"))/attr(y,"scaled:scale")))
+  print(c("In-half:", contraction, "Cross-half:", c))
   return( c(contraction, c) )
+}
+
+
+plot_cmds <- function(conversion.scaled, k){
+  dist.matrix<-dist(conversion.scaled, method = "euclidean")
+  fit <- cmdscale(dist.matrix, eig=TRUE, k=2) # k is the number of dim
+  x <- fit$points[,1]
+  y <- fit$points[,2]
+  png(filename=sprintf("plot/MDS_%s.png", k),width = 720, height = 480)
+  plot(x, y, type="n", xlab="Coordinate 1", ylab="Coordinate 2", main="")
+  text(x, y, labels = 1:length(x), cex=1.1)
+  dev.off()
+}
+
+plot_pca <- function(conversion.scaled, k){
+  autoplot(prcomp(conversion.scaled),loadings = F,label = TRUE, shape = FALSE, label.size = 4)
+  ggsave(filename=sprintf("plot/PCA_%s.png", k),width = 7.2, height = 4.8)
+  # dev.off()
 }
